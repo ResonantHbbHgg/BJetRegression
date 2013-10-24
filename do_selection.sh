6de7974ae443eccc0cccf98c7339621679189bc6
Olivier <olivier.bondu@gmail.com>
2013-10-24 14:10:16 +0200

    Adding backgrounds to the list of samples to apply the selection on

--- do_selection.sh
+++ do_selection.sh
@@ -8,11 +8,13 @@ eospath="/store/cmst3/user/obondu/H2GGLOBE/Radion/trees/"
 
 i=-1
 
+##### DATA
 i=$((${i} + 1))
 infile[${i}]="radion_tree_v06/Data_Full2012.root"
 tree[${i}]="Data"
 typ[${i}]="0"
 
+##### SIGNAL
 i=$((${i} + 1))
 infile[${i}]="radion_tree_v07/Radioon-Graviton_nm.root"
 tree[${i}]="Radion_m300_8TeV_nm"
@@ -38,6 +40,7 @@ infile[${i}]="radion_tree_v07/Radioon-Graviton_nm.root"
 tree[${i}]="Radion_m1500_8TeV_nm"
 typ[${i}]="-1500"
 
+##### SM Higgs
 i=$((${i} + 1))
 infile[${i}]="radion_tree_v07/SMHiggs.root"
 tree[${i}]="ggh_m125_minlo_8TeV"
@@ -68,24 +71,70 @@ infile[${i}]="radion_tree_v07/SMHiggs.root"
 tree[${i}]="tth_m125_8TeV"
 typ[${i}]="-1"
 
+##### Diphoton backgrounds
+i=$((${i} + 1))
+infile[${i}]="radion_tree_v06/Backgrounds.root"
+tree[${i}]="qcd_30_8TeV_ff"
+typ[${i}]="1"
+
+i=$((${i} + 1))
+infile[${i}]="radion_tree_v06/Backgrounds.root"
+tree[${i}]="qcd_40_8TeV_ff"
+typ[${i}]="1"
+
+i=$((${i} + 1))
+infile[${i}]="radion_tree_v06/Backgrounds.root"
+tree[${i}]="qcd_30_8TeV_pf"
+typ[${i}]="1"
+
+i=$((${i} + 1))
+infile[${i}]="radion_tree_v06/Backgrounds.root"
+tree[${i}]="qcd_40_8TeV_pf"
+typ[${i}]="1"
+
+i=$((${i} + 1))
+infile[${i}]="radion_tree_v06/Backgrounds.root"
+tree[${i}]="gjet_20_8TeV_pf"
+typ[${i}]="1"
+
+i=$((${i} + 1))
+infile[${i}]="radion_tree_v06/Backgrounds.root"
+tree[${i}]="gjet_40_8TeV_pf"
+typ[${i}]="1"
+
+i=$((${i} + 1))
+infile[${i}]="radion_tree_v06/Backgrounds.root"
+tree[${i}]="diphojet_sherpa_8TeV"
+typ[${i}]="1"
+
+i=$((${i} + 1))
+infile[${i}]="radion_tree_v06/Backgrounds.root"
+tree[${i}]="DYJetsToLL"
+typ[${i}]="1"
+
+
 itot=${i}
+masscutsuffix[0]="noMassCut"
 regsuffix[0]="noRegression"
 regsuffix[2]="PhilRegr1017"
+imasscut=0
 
-for ireg in `echo "0 2"`
+for ireg in `echo "2 0"`
 do
-	folder="${today}_selection_${regsuffix[${ireg}]}_${version}"
+	folder="${today}_selection_${regsuffix[${ireg}]}_${masscutsuffix[${imasscut}]}_${version}"
 	mkdir -p ${folder}
 	for isample in `seq 0 ${itot}`
 	do
 		echo -e "isample= ${isample}\tinfile= ${infile[${isample}]}\ttree= ${tree[${isample}]}\ttyp= ${typ[${isample}]}\tireg= ${ireg}"
-		file="${tree[${isample}]}_${regsuffix[${ireg}]}_${version}"
+		file="${tree[${isample}]}_${regsuffix[${ireg}]}_${masscutsuffix[${imasscut}]}_${version}"
+
 ./selection.exe \
 -i ${eosprefix}${eospath}${infile[${isample}]} \
 -t ${tree[${isample}]} \
 -o ${folder}/${file}.root \
---type ${typ[${isample}]} \
 -r ${ireg} \
+--type ${typ[${isample}]} \
+--applyMassCuts ${imasscut} \
 &> ${folder}/${file}.eo
 
 	done # isample
