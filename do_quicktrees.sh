b7749acb0b40b663f3ea54d6c3f3acbb682b3a8c
Olivier <olivier.bondu@gmail.com>
2013-10-30 15:31:05 +0100

    Updating do_quicktrees.sh script to produce mggjj limit trees with corrected weights as well

--- do_quicktrees.sh
+++ do_quicktrees.sh
@@ -63,54 +63,54 @@ done # massCutVersion
 done # data# do mgg trees
 
 
-#for data in `echo "data signal"`
-#do
-#	for mass in `echo "300 500 700 1000"`
-#	do
-#		for cutLevel in `echo "0"`
-#		do
-#			for fitStrategy in `echo "mggjj"`
-#			do
-#				for whichJet in `echo "base reg kin regkin"`	
-#				do
-#
-#					if [[ "${data}" == "data" ]]
-#					then
-#						intree="Data"
-#						removeUndefinedBtagSF=0
-#						type_=0
-#					elif [[ "${data}" == "signal" ]]
-#					then
-#						intree="Radion_m${mass}_8TeV_nm"
-#						removeUndefinedBtagSF=1
-#						type_=0
-#					fi
-#					if [[ "${whichJet}"	== "base" ]]
-#					then
-#						infile="2013-10-28_selection_noRegression_noMassCut_v01/${intree}_noRegression_noMassCut_v01.root"
-#					else
-#						infile="2013-10-28_selection_PhilRegr1028_noMassCut_v01/${intree}_PhilRegr1028_noMassCut_v01.root"
-#					fi
-#					outfolder="${version}_${whichJet}_${fitStrategy}_${cutLevel}"
-#					mkdir -p ${outfolder}
-#					outfile="${today}-${intree}_m${mass}.root"
-#					outtree="TCVARS"
-#
-#
-#./quickTrees.exe \
-#-i ${infile} \
-#-it ${intree} \
-#-o ${outfolder}/${outfile} \
-#-cutLevel ${cutLevel} \
-#-m ${mass} \
-#-fs ${fitStrategy} \
-#-wj ${whichJet} \
-#--removeUndefinedBtagSF ${removeUndefinedBtagSF} \
-#--type ${type_} \
-#| tee ${outfolder}/${today}-${intree}_m${mass}.log
-#
-#				done # whichJet
-#			done # fitStrategy
-#		done # cutLevel
-#	done # mass
-#done # data
+for data in `echo "data signal"`
+do
+	for mass in `echo "300 500 700 1000"`
+	do
+		for cutLevel in `echo "0"`
+		do
+			for fitStrategy in `echo "mggjj"`
+			do
+				for whichJet in `echo "base reg kin regkin"`	
+				do
+
+					if [[ "${data}" == "data" ]]
+					then
+						intree="Data"
+						removeUndefinedBtagSF=0
+						type_=0
+					elif [[ "${data}" == "signal" ]]
+					then
+						intree="Radion_m${mass}_8TeV_nm"
+						removeUndefinedBtagSF=1
+						type_=0
+					fi
+					if [[ "${whichJet}"	== "base" ]]
+					then
+						infile="2013-10-28_selection_noRegression_noMassCut_v01/${intree}_noRegression_noMassCut_v01.root"
+					else
+						infile="2013-10-28_selection_PhilRegr1028_noMassCut_v01/${intree}_PhilRegr1028_noMassCut_v01.root"
+					fi
+					outfolder="${version}_${whichJet}_${fitStrategy}_${cutLevel}"
+					mkdir -p ${outfolder}
+					outfile="${today}-${intree}_m${mass}.root"
+					outtree="TCVARS"
+
+
+./quickTrees.exe \
+-i ${infile} \
+-it ${intree} \
+-o ${outfolder}/${outfile} \
+-cutLevel ${cutLevel} \
+-m ${mass} \
+-fs ${fitStrategy} \
+-wj ${whichJet} \
+--removeUndefinedBtagSF ${removeUndefinedBtagSF} \
+--type ${type_} \
+| tee ${outfolder}/${today}-${intree}_m${mass}.log
+
+				done # whichJet
+			done # fitStrategy
+		done # cutLevel
+	done # mass
+done # data
