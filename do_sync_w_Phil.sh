dd2fffdd661776b8f5c9c9277e2f5ce80e5128aa
Olivier <olivier.bondu@gmail.com>
2013-10-30 21:37:04 +0100

    Adding command to run sync with Phil

---
+++ do_sync_w_Phil.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/selection.exe -i root://eoscms//eos/cms/store/cmst3/user/obondu/H2GGLOBE/Radion/trees/radion_tree_v07/Radioon-Graviton_nm.root -t Radion_m300_8TeV_nm -o test.root --type -0 -r 0 --applyMassCuts 0
+
+
