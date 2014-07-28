23f4452c53ac9196984b5e2838db41fa7ec2d322
Olivier <olivier.bondu@gmail.com>
2014-07-28 17:37:35 +0200

    Updating CS weight file

--- do_weightsForControlSample.sh
+++ do_weightsForControlSample.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-selectionDate="2014-06-03"
-selectionVersion="v11"
+selectionDate="2014-06-25"
+selectionVersion="v15"
 output="scales_2D_pt_data_4GeVbinning"
 
 #./selection.exe --inputfile root://eoscms//eos/cms/store/cmst3/user/obondu/H2GGLOBE/Radion/trees/radion_tree_v08/Data_Full2012.root --inputtree Data --outputtree Data --outputfile ${selectionDate}_selection_noRegression_noMassCut_${selectionVersion}/Data_noRegression_noMassCut_controlSample_${selectionVersion}.root --numberOfRegressionFiles 0 --type 0 --removeUndefinedBtagSF 0 --applyMassCuts 0 --applyPhotonIDControlSample 1 &> ${selectionDate}_selection_noRegression_noMassCut_${selectionVersion}/Data_noRegression_noMassCut_controlSample_${selectionVersion}.eo
